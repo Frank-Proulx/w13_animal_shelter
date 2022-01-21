1f6b22b44b6c89b658001be29dc6827d3be791b3
Frank-Proulx <frankproulx7@gmail.com>
2022-01-21 11:06:39 -0800

    add specs for get route having passed tests

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,17 +1,9 @@
 FactoryBot.define do
-  factory(:cat) do
+  factory(:animal) do
     name {Faker::Creature::Cat.name}
-    species {"Cat"}
+    species {'Cat'}
     age {rand(1..15)}
     breed {Faker::Creature::Cat.breed}
-    sex {Faker::Gender.binary_type}
-  end
-
-  factory(:dog) do
-    name {Faker::Creature::Dog.name}
-    species {"Dog"}
-    age {rand(1..15)}
-    breed {Faker::Creature::Dog.breed}
     sex {Faker::Gender.binary_type}
   end
 end
---
+++ spec/requests/get_animals_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+describe "get all animals route", :type => :request do
+  let!(:animals) { FactoryBot.create_list(:animal, 8)}
+
+  it 'returns all animals' do
+    get '/animals'
+    expect(JSON.parse(response.body).size).to eq(8)
+  end
+  
+  it 'returns the first 5 animals' do
+    get '/animals?page=1'
+    expect(JSON.parse(response.body).size).to eq(5)
+  end
+
+  it 'returns the last 3 animals' do
+    get '/animals?page=2'
+    expect(JSON.parse(response.body).size).to eq(3)
+  end
+  
+  it 'returns status code 200' do
+    get '/animals'
+    expect(response).to have_http_status(:success)
+  end
+end
